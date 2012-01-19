d0a828b188555908e420784427481da3ace1cf1b
Byron Kropf <b0ykropf@yahoo.com>
2012-01-18 21:08:47 -0800

    More style changes and added 3anycopy.c which takes a buffer size as a third argument.

--- 3anycopy.c
+++ 3anycopy.c
@@ -10,20 +10,18 @@
 #define FALSE 0
 #define TRUE !FALSE
 
-#ifndef BUF_SIZE
-#define BUF_SIZE 1024
-#endif
-
 
 int main(int argc,char *argv[]){
 
 	int fd_in, fd_out, open_flags;
 	mode_t file_rights;
 	ssize_t num_read;
-	char buffer[BUF_SIZE];
+	int buff_size;
+	buff_size = atoi(argv[3]);
+	char buffer[buff_size];
 
-	if(argc != 3 )
-	err_exit("Program requires 3 arguments. \n", argv[0]);
+	if(argc != 4 )
+		err_exit("Program requires 3 arguments. \n", argc -1);
 
 // Open Source and Destination files if Destination does not exist create it
 
@@ -31,7 +29,7 @@ int main(int argc,char *argv[]){
 	if(fd_in == -1)
 		err_exit("Could not open source file %s", argv[1]);
 
-	open_flags = O_CREAT | O_WRONLY | O_TRUNC;
+	open_flags = O_CREAT | O_WRONLY | O_TRUNC | O_EXCL;
 	file_rights = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IWOTH;
 	fd_out = open(argv[2], open_flags, file_rights);
 	if(fd_out == -1)
@@ -39,8 +37,8 @@ int main(int argc,char *argv[]){
 
 // Transfer data from Source to Destitation until finished or there is an error
 
-	while((num_read = read(fd_in, buffer, BUF_SIZE)) > 0)
-		if(write(fd_out, buffer, num_read) != num_read){
+	while((num_read = read(fd_in, buffer, buff_size)) > 0)
+		if(write(fd_out, buffer, num_read) != num_read{
 			close(fd_in);
 			close(fd_out);
 			err_exit("Could not write entire buffer.");
